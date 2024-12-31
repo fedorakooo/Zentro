87e903b5965e7bd4351a8b63d4e4ac1dc3c0632c
Alexander Fedorako <fedorakooo@gmail.com>
2024-12-31 19:47:24 +0300

    Add Pydantic models for reviews

---
+++ backend/app/core/schemas/reviews.py
@@ -0,0 +1,32 @@
+from pydantic import BaseModel, conint
+from datetime import date
+from typing import Optional
+
+
+# Shared properties for review, base class for common review attributes
+class ReviewBase(BaseModel):
+    user_id: int
+    product_id: int
+    rating: conint(ge=1, le=5)  # Rating should be between 1 and 5
+    comment: Optional[str] = None
+    created_at: date
+    updated_at: date
+
+
+# Properties to receive via API when updating review details, all are optional
+class ReviewUpdate(BaseModel):
+    rating: Optional[conint(ge=1, le=5)] = None
+    comment: Optional[str] = None
+
+
+# Properties to receive via API when creating a review
+class ReviewCreate(BaseModel):
+    user_id: int
+    product_id: int
+    rating: conint(ge=1, le=5)
+    comment: Optional[str] = None
+
+
+# Main Review model with all properties
+class Review(ReviewBase):
+    id: int
