86382a9c0f19b9843118d3611172d28ff7147f28
Alexander Fedorako <fedorakooo@gmail.com>
2025-01-06 20:56:52 +0300

    Update database settings in config.py

--- backend/app/core/config.py
+++ backend/app/core/config.py
@@ -1,10 +1,30 @@
 import os
 
+from pathlib import Path
+from pydantic import BaseModel
+from pydantic_settings import BaseSettings
 from dotenv import load_dotenv
 
 load_dotenv()
 
-DB_HOST = os.environ.get("DB_HOST")
-DB_NAME = os.environ.get("DB_NAME")
-DB_USER = os.environ.get("DB_USER")
-DB_PASS = os.environ.get("DB_PASS")
+# Project base directory
+BASE_DIR = Path(__file__).parent.parent
+
+
+class DbSettings(BaseModel):
+    host: str = os.environ.get("DB_HOST", "localhost")
+    name: str = os.environ.get("DB_NAME", "default_db")
+    user: str = os.environ.get("DB_USER", "user")
+    password: str = os.environ.get("DB_PASS", "password")
+
+    # Forming a URL for connecting to the database
+    url: str = f"postgresql+asyncpg://{user}:{password}@{host}/{name}"
+    echo: bool = True
+
+
+class Settings(BaseSettings):
+
+    db: DbSettings = DbSettings()
+
+
+settings = Settings()
--- backend/app/core/db.py
+++ backend/app/core/db.py
@@ -1,9 +1,9 @@
 from sqlalchemy.ext.asyncio import create_async_engine, async_sessionmaker
 from sqlalchemy.orm import declarative_base
 
-from app.core.config import DB_USER, DB_PASS, DB_HOST, DB_NAME
+from app.core.config import settings
 
-DATABASE_URL = f"postgresql+asyncpg://{DB_USER}:{DB_PASS}@{DB_HOST}/{DB_NAME}"
+DATABASE_URL = settings.db.url
 
 engine = create_async_engine(DATABASE_URL)
 async_session_maker = async_sessionmaker(engine, expire_on_commit=False)
