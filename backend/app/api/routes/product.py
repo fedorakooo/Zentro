fa4878d1ce2f3f98fac3c056852d11b1c1505595
Alexander Fedorako <fedorakooo@gmail.com>
2025-01-17 19:06:14 +0300

    Implement add product to cart functionality

--- backend/app/api/routes/product.py
+++ backend/app/api/routes/product.py
@@ -1,7 +1,11 @@
 from fastapi import APIRouter
+from fastapi.params import Depends
 
+from app.core.schemas.cart import CartItemRequest
 from app.core.schemas.products import Product
-from app.services.product import get_product_by_id
+from app.core.schemas.users import User
+from app.services.product import get_product_by_id, add_item_to_cart
+from app.services.user import get_current_active_auth_user
 
 router = APIRouter(tags=["Product"], prefix="/products")
 
@@ -10,3 +14,17 @@ router = APIRouter(tags=["Product"], prefix="/products")
 async def get_product(product_id: int) -> Product:
     product: Product = await get_product_by_id(product_id)
     return product
+
+
+@router.post("/{product_id}")
+async def add_product_to_cart(
+        product_id: int,
+        quantity: int,
+        user: User = Depends(get_current_active_auth_user)
+):
+    cart_item: CartItemRequest = CartItemRequest(
+        user_id=user.id,
+        product_id=product_id,
+        quantity=quantity)
+    result = await add_item_to_cart(cart_item)
+    return result
--- backend/app/services/product.py
+++ backend/app/services/product.py
@@ -2,7 +2,9 @@ from fastapi import HTTPException
 
 from sqlalchemy import select
 
+from app.core.models.cart import CartORM
 from app.core.models.products import ProductORM
+from app.core.schemas.cart import CartItemRequest
 from app.core.schemas.products import Product
 from app.dependencies.db import get_db
 
@@ -20,3 +22,33 @@ async def get_product_by_id(product_id: int) -> Product:
     product = Product.from_orm(product_db)
 
     return product
+
+
+async def add_item_to_cart(cart_item: CartItemRequest):
+    async with get_db() as db:
+        query = select(CartORM).where(
+            CartORM.user_id == cart_item.user_id,
+            CartORM.product_id == cart_item.product_id
+        )
+        result = await db.execute(query)
+
+        cart_item_db = result.scalars().one_or_none()
+
+        if cart_item_db:
+            cart_item_db.quantity += cart_item.quantity
+            return {
+                "message": "Product added to cart"
+            }
+
+        new_cart_item = CartORM(
+            user_id=cart_item.user_id,
+            product_id=cart_item.product_id,
+            quantity=cart_item.quantity
+        )
+        db.add(new_cart_item)
+        db.commit()
+        db.refresh(new_cart_item)
+
+        return {
+            "message": "Product added to cart"
+        }
