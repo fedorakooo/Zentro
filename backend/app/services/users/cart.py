2f8ab28a1ab86819b335b2980e2cca14cee3fa78
Alexander Fedorako <fedorakooo@gmail.com>
2025-01-18 14:07:56 +0300

    Improve function signature with type annotations

--- backend/app/services/users/cart.py
+++ backend/app/services/users/cart.py
@@ -1,3 +1,5 @@
+from typing import List
+
 from sqlalchemy import select
 
 from app.core.schemas.cart import CartItemRequest
@@ -5,13 +7,15 @@ from app.core.models.cart import CartORM
 from app.dependencies.db import get_db
 
 
-async def get_cart_product_id_by_user_id(user_id: int):
+async def get_cart_product_id_by_user_id(user_id: int) -> List[CartItemRequest]:
     async with get_db() as db:
         query = select(CartORM).where(CartORM.user_id == user_id)
         result = await db.execute(query)
         products_db = result.scalars().all()
 
     products = []
+
     for product_db in products_db:
         products.append(CartItemRequest.from_orm(product_db))
+
     return products
