accca1c6dda36005cb1fe2192d8dc2f7873d14fd
Alexander Fedorako <fedorakooo@gmail.com>
2025-01-22 22:27:43 +0300

    Simplify user profile endpoint response

--- backend/app/api/routes/user/profile.py
+++ backend/app/api/routes/user/profile.py
@@ -1,6 +1,5 @@
 from fastapi import APIRouter
 from fastapi.params import Depends
-from fastapi.encoders import jsonable_encoder
 
 from app.core.schemas.users import User
 from app.services.user.user import get_current_active_auth_user
@@ -13,6 +12,6 @@ router.include_router(cart_router)
 router.include_router(products_router)
 
 
-@router.get("/")
+@router.get("/", response_model=User)
 async def get_user_profile(user: User = Depends(get_current_active_auth_user)):
-    return jsonable_encoder(user)
+    return user
