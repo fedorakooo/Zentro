dfd2b5d1850259350bd494ada2336e4857542a20
Alexander Fedorako <fedorakooo@gmail.com>
2025-01-22 22:27:11 +0300

    Correct cart product fetching for user

--- backend/app/core/schemas/cart.py
+++ backend/app/core/schemas/cart.py
@@ -1,10 +1,21 @@
 from pydantic import BaseModel
 
+from app.core.schemas.products import Product
+
 
 class CartItemRequest(BaseModel):
+    product_id: int
+    quantity: int
     user_id: int
+
+
+class CartItemDTO(BaseModel):
     product_id: int
     quantity: int
 
     class Config:
         from_attributes = True
+
+
+class CartItem(Product):
+    quantity: int
--- backend/app/services/user/cart.py
+++ backend/app/services/user/cart.py
@@ -2,20 +2,44 @@ from typing import List
 
 from sqlalchemy import select
 
-from app.core.schemas.cart import CartItemRequest
+from app.core.models.products import ProductORM
+from app.core.schemas.cart import CartItemDTO, CartItem
 from app.core.models.cart import CartORM
+from app.core.schemas.products import Product
 from app.dependencies.db import get_db
 
 
-async def get_cart_product_id_by_user_id(user_id: int) -> List[CartItemRequest]:
+async def get_cart_products_by_user_id(user_id: int) -> List[Product]:
+    products_data: List[CartItemDTO] = await get_cart_product_data_object_by_user_id(user_id)
+
+    cart_items: List[CartItem] = []
+
+    async with get_db() as db:
+        for product_data in products_data:
+            query = select(ProductORM).where(ProductORM.id == product_data.product_id)
+            result = await db.execute(query)
+            product_db = result.scalars().one_or_none()
+
+            product = Product.from_orm(product_db)
+
+            cart_item = CartItem(**product.dict(), quantity=product_data.quantity)
+
+            cart_items.append(cart_item)
+
+    return cart_items
+
+
+async def get_cart_product_data_object_by_user_id(user_id: int) -> List[CartItemDTO]:
     async with get_db() as db:
         query = select(CartORM).where(CartORM.user_id == user_id)
         result = await db.execute(query)
         products_db = result.scalars().all()
 
-    products = []
+        return [
+            CartItemDTO(
+                product_id=product_db.product_id,
+                quantity=product_db.quantity
+            )
+            for product_db in products_db
+        ]
 
-    for product_db in products_db:
-        products.append(CartItemRequest.from_orm(product_db))
-
-    return products
